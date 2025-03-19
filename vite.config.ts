0eafc2bea0bbf9a479f4a8cd666927322727d512
Archit Rohal <rohalarchit@gmail.com>
2025-03-19 22:46:57 +0530

    add 'base' line in vite.config.ts following pedro

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+    server: {
+        host: "::",
+        port: 8080,
     },
-  },
+    plugins: [
+        react(),
+        mode === 'development' &&
+        componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
